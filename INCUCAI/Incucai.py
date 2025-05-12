95f9a5320892afb9fb0c68874d4da71eb4240099
bici-alonso <vvictoriaalonso@gmail.com>
2025-05-12 07:16:35 -0300

    cambios (zoe)

--- INCUCAI/Incucai.py
+++ INCUCAI/Incucai.py
@@ -1,40 +1,8 @@
-'''
-TRABAJO PRACTICO FINAL - LABO DE PROGRAMACION I. 
-SIMULACION SIST. DONACION DE ORGANOS
-
-Alonso Victoria
-Pfeifer Zoe
-
-El INCUCAI se encarga de la coordinación y logística de la donación de tejidos y órganos. 
-Debido a que su sistema quedó desactualizado por el paso del tiempo, le solicitaron realizar una nueva versión dónde tendrá que mejorar la automatización y
-la logística para optimizar la llegada de órganos a los pacientes que se encuentran en la lista de espera. 
-Este consta de una lista de receptores, una lista de donantes y una lista de centros de salud habilitados.
-
-El Sistema tiene que permitir:
-    -Registrar pacientes nuevos (Validando que no se encuentre en otra lista ni se repita).
-    -Quitar de la lista de donantes a aquellos cuyos organos ya han sido utilizados en su totalidad.
-    -Quitar de la lista a un receptor una vez que ha recibido su organo exitosamente.
-    -Buscar por centro de salud los pacientes de la lista de espera.
-    -Buscar un receptor e informar qué prioridad tiene en la lista de espera.
-    -Imprimir listado de pacientes donantes y receptores.
-    -Realizar correctamente todo el proceso de asignación y derivación de un organo a un receptor,
-    contemplando el viaje, la disponibilidad en ese horario de los vehiculos de un centro medico y el tiempo de viaje.
-
-
-para subir los archivos al git:
-
-git add .
-
-git commit -m "Descripción de lo que hiciste"
-
-git push
-
-'''
-
-
-
 #La clase INCUCAI la uso como manager de mis demas clases --> Permite manejar/linkear mis clases y listas
 
+from Paciente import Paciente
+from .Donante import Donante #no me esta dejando importar, como esta en carpetas chat me dijo q le ponga ese punto pero tampoco funciona
+from .Receptor import Receptor #lo dejo asi con todo hecho pero nose si esta funcionando bien porque no puedo correrlo
 
 class Incucai:
     
@@ -67,32 +35,25 @@ class Incucai:
         opcion_don_rec=int(input("A que lista quiere agregar?\n1- Lista Receptores\n2-Lista donantes"))
         
         if opcion_don_rec == 1:
-            estado = 'receptor' #??
-            return estado
-        
+            que_es = 'receptor'
         elif opcion_don_rec == 2:
-            estado = 'donante' #?
-            return estado
-        
+            que_es = 'donante' 
         else:
             print("\nOpcion no valida. El paciente DEBE SER DONANTE O RECEPTOR")
-            opcion_don_rec=int(input("A que lista quiere agregar?\n1- Lista Receptores\n2-Lista donantes"))
-            
+            return
         
-        
-            
-            
-        
-        
+        paciente = Paciente(que_es)
+        paciente.cargar_datos()
 
-    def asignar_lista(self): #esta asignacion deberia estar en la clase paciente y deberia directamente leer el atributo que se hace por carga con la palabra ?
-        numero=int(input("A que lista quiere agregar?\n1- Lista Receptores\n2-Lista donantes"))
-        if numero == 1:
-            for i in self.receptores:
-                return
-    
+        datos = paciente.__dict__  #este dict toma los dAtos que se guardaron en pqciente y los pasa como uno solo, pata guardarlo en la lista
+
+        if que_es == "receptor":
+            Receptor.agregar(datos)
+        else:
+            Donante.agregar(datos)
+
+
+            
     #incucai debe tener una validacion que ningun paciente se repite
     #debo sacar de la lista a pacientes que no tiene mas organos para donar
     #debo sacar a los receptores que tuvieron un transplante exitoso
-
-
