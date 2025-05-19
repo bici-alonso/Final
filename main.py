0ca8fd6b80d261ed7ddc6a215cd71677cac23b06
Zoee <zoenerea2005@gmail.com>
2025-05-19 14:05:04 -0300

    carga pacientes

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ from INCUCAI.Paciente.Receptor import Receptor
 
 def main():
     #a fines de testeo usamos estos objetos que no los hago por carga de datos
-    #paciente= Paciente("Zoe", 46821489, "6/11/2005", "F", 1126485713, "0+", "Favaloro", "Donante") #esta carga deberia resolverse con la funcion agregar
+    paciente= Paciente("Zoe", 46821489, "6/11/2005", "F", 1126485713, 1125365869, "0+", "Favaloro", "Donante") #esta carga deberia resolverse con la funcion agregar
     #paciente2=Paciente("Vicky", 46583726, "25/03/2005", "F", 1135857168, "0+", "Favaloro", "Donante" )
     #paciente.datos_pacientes() #metodo tipo getter
     #paciente2.datos_paciente()
     incucai = Incucai()
+    incucai.clasificar_pac(paciente_exist=paciente)
+
+
+    '''incucai = Incucai()
     incucai.clasificar_pac()
 
     for d in Donante.listar():
@@ -21,7 +25,7 @@ def main():
     
     for r in Receptor.listar():
         print("---------------------lista receptores------------------")
-        print(r)
+        print(r)''' #ESTO ES PARA Q LO CARGUE POR PANTALLA
 
 
 if __name__ == "__main__":
