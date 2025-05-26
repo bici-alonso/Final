dd0c88ab406404187da0cf39aadcfd70c09c1f18
bici-alonso <vvictoriaalonso@gmail.com>
2025-05-26 10:16:18 -0300

    actualizacion requierements

--- INCUCAI/Centros/Cirujanos/Cirujano.py
+++ INCUCAI/Centros/Cirujanos/Cirujano.py
@@ -1,10 +1,10 @@
-'''
-Los cirujanos pueden ser generales o tener especialidades. Si la operación la realiza un cirujano general
-entonces el exito de la operación depende de un valor aleatorio entre 1 y 10, si sale mas de 5 la operación es
-exitosa, 5 o menos falla. Si en cambio tiene especialidades depende del organo a operar. Si el organo influye
-en su especialidad entonces en el valor de entre 1 y 10, basta con que sea un 3 o mayor. Los cirujanos solo
-pueden hacer una operación al día, por lo que, si ya fueron asignados a una operación antes, no pueden ser
-asignados a una nueva.
+''' Los cirujanos pueden ser generales o tener especialidades. 
+Si la operación la realiza un cirujano general entonces el exito de la operación depende de un valor aleatorio entre 1 y 10,
+si sale mas de 5 la operación es exitosa, 5 o menos falla. 
+Si en cambio tiene especialidades depende del organo a operar. 
+Si el organo influye en su especialidad entonces en el valor de entre 1 y 10, basta con que sea un 3 o mayor. 
+Los cirujanos solo pueden hacer una operación al día, por lo que, si ya fueron asignados a una operación antes, 
+no pueden ser asignados a una nueva.
 
 Las especialidades que hay son:
     Cirujano cardiovascular: Tiene especialidad si la operación es de corazón.
@@ -17,7 +17,6 @@ Las especialidades que hay son:
 from abc import ABC, abstractmethod
 from datetime import date, datetime
 
-import random
 
 class Cirujano(ABC):
     def __init__(self, nombre):
@@ -34,3 +33,7 @@ class Cirujano(ABC):
             raise Exception (f"\nCirujano/a {self.nombre} no disponible para otra operacion en el dia.")
         self.ult_operacion=date.today()
         self.operaciones_del_dia.append(datetime.now())
+    
+    @abstractmethod
+    def exito_operacion(self):
+        pass 
